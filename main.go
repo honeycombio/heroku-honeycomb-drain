5a703b1113a87830023772782aa9b77f1eb4c301
David Reid <dreid@dreid.org>
2018-01-08 10:53:40 -0800

    Run go fmt

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/bmizerany/lpx"
+	"github.com/go-logfmt/logfmt"
+	"github.com/honeycombio/libhoney-go"
 	"log"
 	"net/http"
 	"os"
-	"fmt"
-	"bytes"
-	"time"
-	"bufio"
-	"strings"
 	"strconv"
-	"encoding/json"
-	"github.com/go-logfmt/logfmt"
-	"github.com/bmizerany/lpx"
-	"github.com/honeycombio/libhoney-go"
+	"strings"
+	"time"
 )
 
 type LogDrain struct {
-	DebugLogs bool
+	DebugLogs   bool
 	AllowedApps map[string]string
-	AppFormats [][2]string
+	AppFormats  [][2]string
 }
 
 type ToEvent func(message []byte, event *libhoney.Event) bool
 
 var formatterMap = map[string]ToEvent{
 	"logfmt": LogFmtToEvent,
-	"json": JsonToEvent,
-	"raw": RawToEvent,
+	"json":   JsonToEvent,
+	"raw":    RawToEvent,
 	"ignore": IgnoreToEvent,
 }
 
@@ -38,12 +38,12 @@ func JsonToEvent(message []byte, event *libhoney.Event) bool {
 		event.AddField("json_err", err)
 		event.AddField("raw_message", string(message))
 		return true
-	} else {		
+	} else {
 		for k, v := range j {
 			event.AddField(k, v)
 		}
 	}
-	
+
 	return true
 }
 
@@ -59,38 +59,38 @@ func coerceLogFmtValue(value string) interface{} {
 	if err == nil {
 		return outValue
 	}
-	
+
 	outValue, err = strconv.ParseFloat(value, 64)
 	if err == nil {
 		return outValue
 	}
-	
+
 	outValue, err = strconv.ParseUint(value, 10, 64)
 	if err == nil {
 		return outValue
 	}
-	
+
 	outValue, err = strconv.ParseBool(value)
 	if err == nil {
 		return outValue
 	}
-	
+
 	return value
 }
 
 func LogFmtToEvent(message []byte, event *libhoney.Event) bool {
-	d := logfmt.NewDecoder(bytes.NewBuffer(message)) 
+	d := logfmt.NewDecoder(bytes.NewBuffer(message))
 	for d.ScanRecord() {
 		for d.ScanKeyval() {
 			event.AddField(string(d.Key()), coerceLogFmtValue(string(d.Value())))
 		}
-		
+
 		if d.Err() != nil {
-			event.AddField("logfmt_err", d.Err()) 
+			event.AddField("logfmt_err", d.Err())
 			event.AddField("raw_message", string(message))
 		}
 	}
-	
+
 	return true
 }
 
@@ -105,10 +105,10 @@ func IgnoreToEvent(message []byte, event *libhoney.Event) bool {
 
 func (ld *LogDrain) FormatterForHostApp(name string, proc_id string) ToEvent {
 	variants := [2]string{
-		fmt.Sprintf("%s/%s", name, proc_id), 
+		fmt.Sprintf("%s/%s", name, proc_id),
 		fmt.Sprintf("%s/*", name),
 	}
-		
+
 	for _, spec := range ld.AppFormats {
 		for _, variant := range variants {
 			if spec[0] == variant {
@@ -116,7 +116,7 @@ func (ld *LogDrain) FormatterForHostApp(name string, proc_id string) ToEvent {
 			}
 		}
 	}
-	
+
 	return RawToEvent
 }
 
@@ -129,48 +129,48 @@ func (ld *LogDrain) Handle(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 	}
-	
-	builder := libhoney.NewBuilder() 
 
-	lp := lpx.NewReader(bufio.NewReader(req.Body)) 
+	builder := libhoney.NewBuilder()
+
+	lp := lpx.NewReader(bufio.NewReader(req.Body))
 	for lp.Next() {
-		event := builder.NewEvent() 
-		
+		event := builder.NewEvent()
+
 		name, proc_id := string(lp.Header().Name), string(lp.Header().Procid)
-		
-		event.AddField("host", string(lp.Header().Hostname)) 
-		event.AddField("app", name) 
+
+		event.AddField("host", string(lp.Header().Hostname))
+		event.AddField("app", name)
 		event.AddField("proc_id", proc_id)
-		event.AddField("msg_id", string(lp.Header().Msgid)) 
-		
+		event.AddField("msg_id", string(lp.Header().Msgid))
+
 		formatter := ld.FormatterForHostApp(name, proc_id)
-		
+
 		emitEvent := formatter(lp.Bytes(), event)
-			
+
 		if !emitEvent {
 			return
 		}
-		
+
 		ts, err := time.Parse(time.RFC3339, string(lp.Header().Time))
 		if err != nil {
-			event.AddField("time_error", err) 
-			ts = time.Now() 
+			event.AddField("time_error", err)
+			ts = time.Now()
 		}
-		
-		event.Timestamp = ts 
 
-		if(!ld.DebugLogs) {
+		event.Timestamp = ts
+
+		if !ld.DebugLogs {
 			event.Send()
 		} else {
-			eventJson, err := json.Marshal(event) 
+			eventJson, err := json.Marshal(event)
 			if err != nil {
 				log.Println(err)
 				continue
 			}
-			log.Println(string(eventJson)) 
+			log.Println(string(eventJson))
 		}
 	}
-	w.WriteHeader(http.StatusOK) 
+	w.WriteHeader(http.StatusOK)
 }
 
 func makeAppFormats(appFormats string) [][2]string {
@@ -179,23 +179,23 @@ func makeAppFormats(appFormats string) [][2]string {
 		if len(spec) == 0 {
 			log.Fatal("No APP_FORMATS specified.")
 		}
-		
+
 		splitSpec := strings.Split(spec, ":")
-		
+
 		if len(splitSpec) < 2 {
 			log.Fatalf("spec '%s' missing an app or a format", spec)
 		}
 		name, format := splitSpec[0], splitSpec[1]
-				
+
 		appFormatsArray = append(appFormatsArray, [2]string{name, format})
 	}
-	
+
 	return appFormatsArray
 }
 
 func makeAllowedApps(allowedApps string) map[string]string {
 	allowedAppsMap := make(map[string]string)
-	
+
 	for _, creds := range strings.Split(allowedApps, ",") {
 		if creds == "" {
 			continue
@@ -206,7 +206,7 @@ func makeAllowedApps(allowedApps string) map[string]string {
 		}
 		allowedAppsMap[splitCreds[0]] = splitCreds[1]
 	}
-	
+
 	return allowedAppsMap
 }
 
@@ -214,33 +214,33 @@ func main() {
 	allowedApps := os.Getenv("ALLOWED_APPS")
 	port := os.Getenv("PORT")
 	dataSet := os.Getenv("HONEYCOMB_DATASET")
-	writeKey := os.Getenv("HONEYCOMB_WRITE_KEY") 
+	writeKey := os.Getenv("HONEYCOMB_WRITE_KEY")
 	appFormats := os.Getenv("APP_FORMATS")
 
 	if dataSet == "" {
 		dataSet = "heroku-logdrain"
 	}
-	
+
 	libhoney.Init(libhoney.Config{
-  	WriteKey: writeKey,
-  	Dataset: dataSet,
-	}) 
-	defer libhoney.Close() 
+		WriteKey: writeKey,
+		Dataset:  dataSet,
+	})
+	defer libhoney.Close()
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
-	
+
 	if writeKey != "" && allowedApps == "" {
 		log.Fatal("$ALLOWED_APPS must be set if $HONEYCOMB_WRITE_KEY is set.")
 	}
-	
+
 	ld := &LogDrain{
 		writeKey == "",
 		makeAllowedApps(allowedApps),
 		makeAppFormats(appFormats),
 	}
-	
-	http.HandleFunc("/", ld.Handle) 
-	log.Fatal(http.ListenAndServe(":" + port, nil)) 
+
+	http.HandleFunc("/", ld.Handle)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
